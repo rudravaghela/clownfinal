df6493facfaf2558b690c1eabb8bcdcf38fd515f
rudravaghela <72458643+rudravaghela@users.noreply.github.com>
2021-05-11 20:12:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+eye_X = 0;
+eye_Y = 0;
+
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+
+  
+function draw(){
+  image(video,0,0,300,300);
+} 
+
+function take_snapshot(){
+    save("rudra.png");
+}
+
+function gotPoses(results){
+  if (results.length > 0)
+{
+  console.log(results);
+  console.log("right eye x = " +results[0].pose.rightEye.x);
+  console.log("right eye y = "+results[0].pose.rightEye.y);
+  
+}
+}
